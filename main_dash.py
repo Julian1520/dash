f430072a044495d7285fbde1c837089c40d0f27a
jschmiss <julian.schmiss@holidaycheck.com>
2018-12-17 10:31:33 +0100

    First wip dashboard

--- main_dash.py
+++ main_dash.py
@@ -1,15 +1,68 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table_experiments
+import os
+import random
+import datetime
+from postgres_utils import BankingDatabase
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
+database = BankingDatabase(database_name=os.environ.get('DATABASE_BANKING'),
+                           user=os.environ.get('DATABASE_BANKING_USER'),
+                           password=os.environ.get('DATABASE_BANKING_PW'),
+                           host=os.environ.get('DATABASE_BANKING_HOST'),
+                           port=os.environ.get('DATABASE_BANKING_PORT'))
+
+sparkasse_balance = database.read_from_db('''
+select amount from sparkasse_balance where date(date) =
+                                             (select max(date(date)) from sparkasse_balance)
+''')
+
+dkb_balance = database.read_from_db('''
+select amount from dkb_balance where date(date) =
+                                             (select max(date(date)) from dkb_balance)
+''')
+
+cc_balance = database.read_from_db('''
+select amount from credit_card_balance where date(date) =
+                                             (select max(date(date)) from credit_card_balance)
+''')
+
+depot_balance = database.read_from_db('''
+select sum(value) as amount from depot_data
+''')
+
+in_out = database.read_from_db('''
+select * from (
+select date_trunc('month',date(date)),tag, sum(amount) as amount from sparkasse_transactions where tag != 'Internal transaction' group by 1,2
+union all
+select date_trunc('month',date(date)),tag, sum(amount) as amount from dkb_transactions where tag != 'Internal transaction' group by 1,2
+union all
+select date_trunc('month',date(value_date)),tag, sum(amount) as amount from credit_card_data where tag != 'Internal transaction' group by 1,2)a
+-- where date_trunc between date('2018-07-01') AND date('{}')
+''')
+
+transactions = database.read_from_db('''
+select amount, applicant_name, date(date), posting_text, purpose, bank_name,tag from dkb_transactions
+union all
+select amount, applicant_name, date(date), posting_text, purpose, bank_name,tag from sparkasse_transactions
+union all
+select amount, description as applicant_name, date(voucher_date), 'NA' as posting_text, 'NA' as purpose, 'NA' as bank_name, tag  from credit_card_data
+order by date desc
+''')
+
+in_out.date_trunc = in_out.date_trunc.apply(lambda x: x.date())
 
 app = dash.Dash()
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': 'rgba(0,0,0,0)',
+    'text': 'rgba(1,1,1,1)'
 }
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='Finance Overview',
         style={
             'textAlign': 'center',
             'color': colors['text']
@@ -23,8 +76,10 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         id='Graph1',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': ['Assets'], 'y': [sparkasse_balance.amount[0]], 'type': 'bar', 'name': 'Sparkasse'},
+                {'x': ['Assets'], 'y': [dkb_balance.amount[0]], 'type': 'bar', 'name': u'DKB'},
+                {'x': ['Assets'], 'y': [cc_balance.amount[0]], 'type': 'bar', 'name': u'CC'},
+                {'x': ['Assets'], 'y': [depot_balance.amount[0]], 'type': 'bar', 'name': u'Depot'}
             ],
             'layout': {
                 'plot_bgcolor': colors['background'],
@@ -34,8 +89,87 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                 }
             }
         }
-    )
+    ),
+
+
+    dcc.DatePickerRange(
+        id="date-picker-in_out",
+        start_date=datetime.datetime(2018, 5, 22),
+        end_date=datetime.datetime(2018, 8, 13),
+        min_date_allowed=datetime.datetime(2018, 5, 22),
+        max_date_allowed=datetime.datetime.now(),
+        end_date_placeholder_text="Select a date"
+    ),
+
+    dcc.Graph(id="in-out-graph"),
+
+    dcc.DatePickerRange(
+        id="date-picker-range",
+        start_date=datetime.datetime(2018, 5, 22),
+        end_date=datetime.datetime(2018, 8, 13),
+        min_date_allowed=datetime.datetime(2018, 5, 22),
+        max_date_allowed=datetime.datetime.now(),
+        end_date_placeholder_text="Select a date"
+    ),
+
+    dash_table_experiments.DataTable(
+        rows=[{}],
+        row_selectable=True,
+        filterable=True,
+        sortable=True,
+        selected_row_indices=[],
+        id='table')
 ])
+
+@app.callback(
+    Output("table", "rows"),
+    [Input("date-picker-range", "start_date"),
+    Input("date-picker-range", "end_date")]
+)
+def update_table(start_date, end_date):
+    return{
+        "columns": [{"name": i, "id": i} for i in transactions.columns],
+        "data": transactions.to_dict("rows"),
+    }
+
+@app.callback(
+    Output("in-out-graph", "figure"),
+    [Input("date-picker-in_out", "start_date"),
+    Input("date-picker-in_out", "end_date")]
+)
+def update_graph(start_date, end_date):
+
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+
+    filtered_df = in_out[(in_out['date_trunc'] > start_date) & (in_out['date_trunc'] < end_date)]
+
+    data = list()
+
+    in_out_dict = (filtered_df.groupby('tag').apply(lambda x: (list(x['amount']), list(x['date_trunc']))).to_dict())
+
+    for key, value in in_out_dict.items():
+        data.append(go.Bar(
+            x=value[1],
+            y=value[0],
+            name=key,
+            marker=go.bar.Marker(
+                color='rgb(' + str(random.randint(0, 255)) + ',' + str(random.randint(0, 255)) + ',' + str(
+                    random.randint(0, 255)) + ')'
+            )
+        ))
+
+    return {
+        "data": data,
+        "layout": go.Layout(
+            autosize=True,
+            title='Net-in-out',
+            showlegend=True,
+            barmode='relative',
+        )
+    }
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
